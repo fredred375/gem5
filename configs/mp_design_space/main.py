21b4ad9c628b24b31e55fb16076521f06f9c1680
Frederick Chu <b08705008@ntu.edu.tw>
2024-11-13 13:57:33 -0600

    misc: format
    Change-Id: I51917b6f7da4d4b65e1ef8cb49257948cdb9083b

---
+++ configs/mp_design_space/main.py
@@ -0,0 +1,109 @@
+# import the m5 (gem5) library created when gem5 is built
+import m5
+
+# import all of the SimObjects
+from m5.objects import *
+
+# Add the common scripts to our path
+m5.util.addToPath("../")
+
+# import the caches which we made
+from caches import *
+
+# import the SimpleOpts module
+from common import SimpleOpts
+
+# Default to running 'hello', use the compiled ISA to find the binary
+# grab the specific path to the binary
+thispath = os.path.dirname(os.path.realpath(__file__))
+default_binary = os.path.join(
+    thispath,
+    "../../",
+    "tests/test-progs/hello/bin/x86/linux/hello",
+)
+
+# Binary to execute
+SimpleOpts.add_option("binary", nargs="?", default=default_binary)
+SimpleOpts.add_option("input", nargs="?", default="")
+
+# Finalize the arguments and grab the args so we can pass it on to our objects
+args = SimpleOpts.parse_args()
+print("args:", args)
+
+# create the system we are going to simulate
+system = System()
+
+# Set the clock frequency of the system (and all of its children)
+system.clk_domain = SrcClockDomain()
+system.clk_domain.clock = "1GHz"
+system.clk_domain.voltage_domain = VoltageDomain()
+
+# Set up the system
+system.mem_mode = "timing"  # Use timing accesses
+system.mem_ranges = [AddrRange("512MB")]  # Create an address range
+
+# Create a simple CPU
+system.cpu = X86O3CPU()
+
+# Create an L1 instruction and data cache
+system.cpu.icache = L1ICache(args)
+system.cpu.dcache = L1DCache(args)
+print(f"L1D size: {system.cpu.dcache.size}")
+print(f"L1D assoc: {system.cpu.dcache.assoc}")
+print(f"L1D response_latency: {system.cpu.dcache.response_latency}")
+
+# Connect the instruction and data caches to the CPU
+system.cpu.icache.connectCPU(system.cpu)
+system.cpu.dcache.connectCPU(system.cpu)
+
+# Create a memory bus, a coherent crossbar, in this case
+system.l2bus = L2XBar()
+
+# Hook the CPU ports up to the l2bus
+system.cpu.icache.connectBus(system.l2bus)
+system.cpu.dcache.connectBus(system.l2bus)
+
+# Create an L2 cache and connect it to the l2bus
+system.l2cache = L2Cache(args)
+system.l2cache.connectCPUSideBus(system.l2bus)
+
+# Create a memory bus
+system.membus = SystemXBar()
+
+# Connect the L2 cache to the membus
+system.l2cache.connectMemSideBus(system.membus)
+
+# create the interrupt controller for the CPU
+system.cpu.createInterruptController()
+system.cpu.interrupts[0].pio = system.membus.mem_side_ports
+system.cpu.interrupts[0].int_requestor = system.membus.cpu_side_ports
+system.cpu.interrupts[0].int_responder = system.membus.mem_side_ports
+
+# Connect the system up to the membus
+system.system_port = system.membus.cpu_side_ports
+
+# Create a DDR3 memory controller
+system.mem_ctrl = MemCtrl()
+system.mem_ctrl.dram = DDR3_1600_8x8()
+system.mem_ctrl.dram.range = system.mem_ranges[0]
+system.mem_ctrl.port = system.membus.mem_side_ports
+
+system.workload = SEWorkload.init_compatible(args.binary)
+
+# Create a process for a simple "Hello World" application
+process = Process()
+# Set the command
+# cmd is a list which begins with the executable (like argv)
+process.cmd = [args.binary, args.input]
+# Set the cpu to use the process as its workload and create thread contexts
+system.cpu.workload = process
+system.cpu.createThreads()
+
+# set up the root SimObject and start the simulation
+root = Root(full_system=False, system=system)
+# instantiate all of the objects we've created above
+m5.instantiate()
+
+print(f"Beginning simulation!")
+exit_event = m5.simulate(10000000000000)
+print(f"Exiting @ tick {m5.curTick()} because {exit_event.getCause()}")
